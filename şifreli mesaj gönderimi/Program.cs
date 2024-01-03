21809f3fb76a0cac07612a034ab4cf91c9670b51
UĞURCAN <UURCAN@LAPTOP-SAB3RQO3>
2024-01-03 22:43:42 +0300

    Proje dosyası ekle.

---
+++ şifreli mesaj gönderimi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace şifreli_mesaj_gönderimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("mesajınzı giriniz");
+            string a = Console.ReadLine();
+            Console.WriteLine("sifrelenmiş mesaj");
+            char[] sifreliMesaj = new char[a.Length];
+            for (int i = 0; i < sifreliMesaj.Length; i++)
+            {
+                sifreliMesaj[i] = Convert.ToChar((int)a[i] + 60);
+                Console.Write(Convert.ToChar((int)a[i] + 60));
+            }
+            Console.WriteLine("\nDeşifreEdilimiş mesaj");
+            for (int i = 0; i < sifreliMesaj.Length; i++)
+            {
+                Console.Write((char)((int)sifreliMesaj[i] - 60));
+            }
+            Console.ReadKey();
+        }
+    }
+}
